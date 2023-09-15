72a822246b8bea72c09c9e3020942e221fc839be
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-09-15 10:08:43 +0600

    Configure Align Dental Care

---
+++ fixtures/basepages.ts
@@ -0,0 +1,45 @@
+import LoginPage from '@pages/Login.page';
+const test = baseTest.extend<{
+
+}>(
+    {
+        // loginPage: async ({page }, use) => {
+        //             await use(new LoginPage(page));
+        //         },
+        //         eventsPage : async({page},use) =>{
+        //             await use (new EventsPage(page))
+        //         },
+        //         clientsPage : async({page},use) =>{
+        //             await use (new companyPage(page))
+        //         },
+        //         employeeuserPage : async({page},use) =>{
+        //             await use (new companyPage(page))
+        //         },
+        //         brokerPage : async({page},use) =>{
+        //             await use (new brokerPage(page))
+        //         },
+        //         AlignwellPage : async({page},use) =>{
+        //             await use (new AlignwelluserPage(page))
+        //         },
+        //         ContactorPage : async({page},use) =>{
+        //             await use (new ContractorPage(page))
+        //         },
+        //         HealthcareProviderPage : async({page},use) =>{
+        //             await use (new HealthcareProviderPage(page))
+        //         },
+        //         companyPage : async({page},use) =>{
+        //             await use (new companyPage(page))
+        //         },
+        //         masteradminPage : async({page},use) =>{
+        //             await use (new masteradminPage(page))
+        //         },
+        //         standardPage : async({page},use) =>{
+        //             await use (new standardPage(page))
+        //         },
+        //         vendorPage : async({page},use) =>{
+        //             await use (new vendorPage(page))
+        //         }
+            },
+)
+export default test;
+export const expect = test.expect;
